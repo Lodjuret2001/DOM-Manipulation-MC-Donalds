b61c333a1a82647b4447f59863b07e47997520fc
Pontus <minecraftpontus@gmail.com>
2023-12-05 21:25:26 +0100

    Const variablar med elemen

---
+++ script.js
@@ -0,0 +1,11 @@
+//alla element som jag behöver i const
+
+const a = document.createElement("a");
+const p = document.createElement("p");
+const img = document.createElement("img");
+const h1 = document.createElement("h1");
+const button = document.createElement("button");
+const h4 = document.createElement("h4");
+
+//----------------------------------------//
+
