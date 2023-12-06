3f8cb7c270ddf05ca2bbaa51b9078dc1f5e80526
Pontus <minecraftpontus@gmail.com>
2023-12-06 12:12:30 +0100

    Made snippets and comment rules in script.js

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-//alla element som jag behöver i const
+//Snippet = "ad"
+// const aName = document.createElement('a');
+// aName.href = '';
+// aName.textContent = 'text';
+// divName.appendChild(aName);
 
-const a = document.createElement("a");
-const p = document.createElement("p");
-const img = document.createElement("img");
-const h1 = document.createElement("h1");
-const button = document.createElement("button");
-const h4 = document.createElement("h4");
+//Snippet = "dd"
+// const divName = document.createElement('div');
+// divName.classList = 'divName';
+// divName.textContent = '';
+// document.body.appendChild(divName);
 
-//----------------------------------------//
+//Snippet = "pd"
+// const pName = document.createElement('p');
+// pName.textContent = '';
+// parent.appendChild(pName);
+
+//Snippet = "imgd"
+// const imgName = document.createElement('img');
+// imgName.src = '#';
+// imgName.classList = 'imgName';
+// imgName.alt = 'imgName';
+// parent.appendChild(imgName);
+
+//Snippet = "btnd"
+// const btnName = document.createElement('button');
+// btnName.textContent = 'btnName';
+// btnName.classList = 'btnName';
+// parent.appendChild(btnName);
+
+//Snippet = "bcd"
+//----------------Start ------------------------//
+
+
+//-------------------Start divName---------------------// <== start new document.body with comment!
 
